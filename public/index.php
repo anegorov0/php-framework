97d24e344e1588f9195859ae89f5f94890374caa
Andrey Egorov <signyts@gmail.com>
2026-03-03 16:48:54 +0300

    add response class

--- public/index.php
+++ public/index.php
@@ -3,7 +3,10 @@
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 use Sadas\Framework\http\Request;
+use Sadas\Framework\http\Response;
 
 $request = Request::createFromGlobals();
 
-dd($request);
+$content = '<h1>Hello, World! </h1>';
+$response = new Response($content, 200, []);
+$response->send();
