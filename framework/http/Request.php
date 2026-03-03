8ee9b33a262ee919bb1afeb279c78ca3522e3d49
Andrey Egorov <signyts@gmail.com>
2026-03-03 16:39:05 +0300

    add request class

---
+++ framework/http/Request.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Sadas\Framework\http;
+
+class Request
+{
+
+    public function __construct(
+        private readonly array $getParams,
+        private readonly array $postParams,
+        private readonly array $cookieParams,
+        private readonly array $fileParams,
+        private readonly array $serverParams,
+    )
+    {
+
+    }
+
+    public static function createFromGlobals(): static
+    {
+        return new static($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,3 +1,9 @@
 <?php
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+use Sadas\Framework\http\Request;
+
+$request = Request::createFromGlobals();
+
+dd($request);
